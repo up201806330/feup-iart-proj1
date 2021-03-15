b60b086abcf8ef88559f30027b7a01195491eb60
Bernardo Ferreira <bernardoferreira2013@gmail.com>
2021-03-15 15:58:27 +0000

    added work specification to report

---
+++ build/src/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bernardo/Desktop/IART/Trabalho Prático/src/GameboardModel.cpp" "/home/bernardo/Desktop/IART/Trabalho Prático/build/src/CMakeFiles/main.dir/GameboardModel.cpp.o"
+  "/home/bernardo/Desktop/IART/Trabalho Prático/src/GameboardView.cpp" "/home/bernardo/Desktop/IART/Trabalho Prático/build/src/CMakeFiles/main.dir/GameboardView.cpp.o"
+  "/home/bernardo/Desktop/IART/Trabalho Prático/src/ScoreboardModel.cpp" "/home/bernardo/Desktop/IART/Trabalho Prático/build/src/CMakeFiles/main.dir/ScoreboardModel.cpp.o"
+  "/home/bernardo/Desktop/IART/Trabalho Prático/src/ScoreboardView.cpp" "/home/bernardo/Desktop/IART/Trabalho Prático/build/src/CMakeFiles/main.dir/ScoreboardView.cpp.o"
+  "/home/bernardo/Desktop/IART/Trabalho Prático/src/TerminalGUI.cpp" "/home/bernardo/Desktop/IART/Trabalho Prático/build/src/CMakeFiles/main.dir/TerminalGUI.cpp.o"
+  "/home/bernardo/Desktop/IART/Trabalho Prático/src/TerminalGUIColor.cpp" "/home/bernardo/Desktop/IART/Trabalho Prático/build/src/CMakeFiles/main.dir/TerminalGUIColor.cpp.o"
+  "/home/bernardo/Desktop/IART/Trabalho Prático/src/TerminalGUIFile.cpp" "/home/bernardo/Desktop/IART/Trabalho Prático/build/src/CMakeFiles/main.dir/TerminalGUIFile.cpp.o"
+  "/home/bernardo/Desktop/IART/Trabalho Prático/src/main.cpp" "/home/bernardo/Desktop/IART/Trabalho Prático/build/src/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
