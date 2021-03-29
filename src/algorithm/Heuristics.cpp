a94b5580b7ac12c8b12643b5ec601dc89a2be48f
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-03-29 16:55:08 +0100

    More fixes

--- src/algorithm/Heuristics.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright (C) 2021 Diogo Rodrigues, Rafael Ribeiro, Bernardo Ferreira
-// Distributed under the terms of the GNU General Public License, version 3
-
-#include "algorithm/heuristics/Heuristic.h"
-
-Heuristic::~Heuristic() = default;
--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,19 @@
 
 
 #include "view/gui/TerminalGUIColor.h"
+
 #include "controller/state/State.h"
 
 using namespace std;
 
-
 int main(){
     TerminalGUI *terminal = new TerminalGUIColor();
     State::initializeStates(terminal);
-    
+
     State *currentState = State::mainMenuState;
     while(currentState != nullptr){
         currentState = currentState->run();
     }
-
+    
     return 0;
 }
