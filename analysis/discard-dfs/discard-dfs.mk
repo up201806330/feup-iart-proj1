bfd3af5bde2ebcbf32719ce4c071f37ebb1f9668
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-04-02 20:29:40 +0100

    More changes

--- analysis/discard-dfs/discard-dfs.mk
+++ analysis/discard-dfs/discard-dfs.mk
@@ -25,18 +25,18 @@ all: discard-dfs-math.csv
 
 discard-dfs-math.csv: discard-dfs.csv
 	head -n 1 discard-dfs.csv > discard-dfs-math.csv
-	cat discard-dfs.csv | tail -n +2 | awk -F , -v OFS=, '{print $$1,$$2,$$3,$$4,$$20/$$5,$$6/1000000,$$7/$$10,$$20/$$8,$$9/1000000,$$10/1000000,($$11==""?"-":$$20/$$11),($$12==""?"-":$$12/1000000),($$13==""?"-":$$13/$$10),$$20/$$14,$$15/1000000,$$16/$$10,$$20/$$17,$$18/1000000,$$19/$$10,$$20,$$21/1000000,$$22/$$10,$$20/$$23,$$24/1000000,$$25/$$10,$$20/$$26,$$27/1000000,$$28/$$10,$$20/$$29,$$30/1000000,$$31/$$10}' >> discard-dfs-math.csv
+	cat discard-dfs.csv | tail -n +2 | awk -F , -v OFS=, '{print $$1,$$2,$$3,$$4,$$20/$$5,$$6/1000000,$$7/$$10,$$20/$$8,$$9/1000000,$$10/1000000,($$11==""?"-":$$20/$$11),($$12==""?"-":$$12/1000000),($$13==""?"-":$$13/$$10),$$20/$$14,$$15/1000000,$$16/$$10,$$20/$$17,$$18/1000000,$$19/$$10,$$20,$$21/1000000,$$22/$$10}' >> discard-dfs-math.csv
 
-discard-dfs.csv: dfs.csv bfs.csv iterative-deepening.csv dfs-greedy-admissible.csv greedy-admissible.csv astar-admissible.csv dfs-greedy-fh.csv dfs-fh.csv astar-fh.csv
+discard-dfs.csv: dfs.csv bfs.csv iterative-deepening.csv dfs-greedy-admissible.csv greedy-admissible.csv astar-admissible.csv #dfs-greedy-fh.csv dfs-fh.csv astar-fh.csv
 	csvcut -c 5,6,7 bfs.csv                     > bfs-cut.csv
 	csvcut -c 5,6,7 iterative-deepening.csv     > iterative-deepening-cut.csv
 	csvcut -c 5,6,7 dfs-greedy-admissible.csv   > dfs-greedy-admissible-cut.csv
 	csvcut -c 5,6,7 greedy-admissible.csv       > greedy-admissible-cut.csv
 	csvcut -c 5,6,7 astar-admissible.csv        > astar-admissible-cut.csv
-	csvcut -c 5,6,7 dfs-greedy-fh.csv           > dfs-greedy-fh-cut.csv
-	csvcut -c 5,6,7 dfs-fh.csv                  > dfs-fh-cut.csv
-	csvcut -c 5,6,7 astar-fh.csv                > astar-fh-cut.csv
-	paste -d , dfs.csv bfs-cut.csv iterative-deepening-cut.csv dfs-greedy-admissible-cut.csv greedy-admissible-cut.csv astar-admissible-cut.csv dfs-greedy-fh-cut.csv dfs-fh-cut.csv astar-fh-cut.csv > discard-dfs.csv
+#	csvcut -c 5,6,7 dfs-greedy-fh.csv           > dfs-greedy-fh-cut.csv
+#	csvcut -c 5,6,7 dfs-fh.csv                  > dfs-fh-cut.csv
+#	csvcut -c 5,6,7 astar-fh.csv                > astar-fh-cut.csv
+	paste -d , dfs.csv bfs-cut.csv iterative-deepening-cut.csv dfs-greedy-admissible-cut.csv greedy-admissible-cut.csv astar-admissible-cut.csv > discard-dfs.csv
 	sed -i 's/-//g' discard-dfs.csv
 	sed -i 's/_//g' discard-dfs.csv
 	rm bfs-cut.csv
@@ -44,9 +44,9 @@ discard-dfs.csv: dfs.csv bfs.csv iterative-deepening.csv dfs-greedy-admissible.c
 	rm dfs-greedy-admissible-cut.csv
 	rm greedy-admissible-cut.csv
 	rm astar-admissible-cut.csv
-	rm dfs-greedy-fh-cut.csv
-	rm dfs-fh-cut.csv
-	rm astar-fh-cut.csv
+#	rm dfs-greedy-fh-cut.csv
+#	rm dfs-fh-cut.csv
+#	rm astar-fh-cut.csv
 
 dfs.csv:
 	$(call run_analysis,dfs,dfs)
@@ -66,11 +66,11 @@ greedy-admissible.csv:
 astar-admissible.csv:
 	$(call run_analysis,informed admissible astar,astar-admissible)
 
-dfs-greedy-fh.csv:
-	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon dfs-greedy,dfs-greedy-fh)
+# dfs-greedy-fh.csv:
+# 	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon dfs-greedy,dfs-greedy-fh)
 
-dfs-fh.csv:
-	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon greedy,dfs-fh)
+# dfs-fh.csv:
+# 	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon greedy,dfs-fh)
 
-astar-fh.csv:
-	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon astar,astar-fh)
+# astar-fh.csv:
+# 	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon astar,astar-fh)
