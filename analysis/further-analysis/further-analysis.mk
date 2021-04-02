3204c769254b569215f3a2ebac09d913c9d8f6f0
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-04-02 20:18:08 +0100

    Changes

--- analysis/further-analysis/further-analysis.mk
+++ analysis/further-analysis/further-analysis.mk
@@ -1,19 +1,19 @@
 MAIN=../../main
 NRUNS=1
 
-define run_analysis
+define run_analysis_short
 	echo "alg,nTubes,tubeH,nColors,seed,nMoves,mem_b,t_ns" > $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 0 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 1 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 2 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 3 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 4 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 5 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 6 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 7 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 8 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	$(MAIN) cli $(NRUNS) 2 4 1 9 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
-	
+	$(MAIN) cli $(NRUNS) 2 4 1  0 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1  1 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1  2 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1  3 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1  4 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1  6 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1  7 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1  8 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1  9 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1 10 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+
 	$(MAIN) cli $(NRUNS) 3 4 1 0 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
 	$(MAIN) cli $(NRUNS) 3 4 1 1 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
 	$(MAIN) cli $(NRUNS) 3 4 1 2 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
@@ -70,48 +70,73 @@ define run_analysis
 	$(MAIN) cli $(NRUNS) 7 4 3 9 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
 endef
 
-all: bfs-processed.csv dfs-greedy-admissible-processed.csv greedy-admissible-processed.csv astar-admissible-processed.csv dfs-greedy-fh-processed.csv dfs-fh-processed.csv astar-fh-processed.csv
+define run_analysis
+	$(call run_analysis_short,$1,$2,$3)
+
+	$(MAIN) cli $(NRUNS) 8 4 4 0 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 1 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 2 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 3 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 4 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 5 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 6 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 7 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 8 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 8 4 4 9 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+
+	$(MAIN) cli $(NRUNS) 9 4 4 0 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 1 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 2 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 3 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 4 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 5 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 6 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 7 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 8 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+	$(MAIN) cli $(NRUNS) 9 4 4 9 $(1) | awk '{print "$2 ,"$$1}' >> $(2).csv
+endef
+
+all: bfs-processed.csv dfs-greedy-admissible-processed.csv greedy-admissible-processed.csv astar-admissible-processed.csv #dfs-greedy-fh-processed.csv greedy-fh-processed.csv astar-fh-processed.csv
 
 bfs-processed.csv: further-analysis-processed.csv
-	csvgrep -c 1 -m "bfs" further-analysis-processed.csv > $@
+	csvgrep -c 1 -r "^bfs$$" further-analysis-processed.csv > $@
 
 dfs-greedy-admissible-processed.csv: further-analysis-processed.csv
-	csvgrep -c 1 -m "dfs-greedy-admissible" further-analysis-processed.csv > $@
+	csvgrep -c 1 -r "^dfs-greedy-admissible$$" further-analysis-processed.csv > $@
 
 greedy-admissible-processed.csv: further-analysis-processed.csv
-	csvgrep -c 1 -m "greedy-admissible" further-analysis-processed.csv > $@
+	csvgrep -c 1 -r "^greedy-admissible$$" further-analysis-processed.csv > $@
 
 astar-admissible-processed.csv: further-analysis-processed.csv
-	csvgrep -c 1 -m "astar-admissible" further-analysis-processed.csv > $@
+	csvgrep -c 1 -r "^astar-admissible$$" further-analysis-processed.csv > $@
 
-dfs-greedy-fh-processed.csv: further-analysis-processed.csv
-	csvgrep -c 1 -m "dfs-greedy-fh" further-analysis-processed.csv > $@
+# dfs-greedy-fh-processed.csv: further-analysis-processed.csv
+# 	csvgrep -c 1 -r "^dfs-greedy-fh$$" further-analysis-processed.csv > $@
 
-dfs-fh-processed.csv: further-analysis-processed.csv
-	csvgrep -c 1 -m "dfs-fh" further-analysis-processed.csv > $@
+# greedy-fh-processed.csv: further-analysis-processed.csv
+# 	csvgrep -c 1 -r "^greedy-fh$$" further-analysis-processed.csv > $@
 
-astar-fh-processed.csv: further-analysis-processed.csv
-	csvgrep -c 1 -m "astar-fh" further-analysis-processed.csv > $@
+# astar-fh-processed.csv: further-analysis-processed.csv
+# 	csvgrep -c 1 -r "^astar-fh$$" further-analysis-processed.csv > $@
 
 process: process.cpp
 	g++ -Wall $< -o $@
 
 further-analysis-processed.csv: process further-analysis.csv
-	./process < further-analysis.csv > /dev/null
 	./process < further-analysis.csv > $@
 
-further-analysis.csv: bfs.csv dfs-greedy-admissible.csv greedy-admissible.csv astar-admissible.csv dfs-greedy-fh.csv dfs-fh.csv astar-fh.csv
+further-analysis.csv: bfs.csv dfs-greedy-admissible.csv greedy-admissible.csv astar-admissible.csv #dfs-greedy-fh.csv greedy-fh.csv astar-fh.csv
 	echo "alg,nTubes,tubeH,nColors,seed,nMoves,mem_b,t_ns" > further-analysis.csv
 	tail -n +2 bfs.csv                   >> further-analysis.csv
 	tail -n +2 dfs-greedy-admissible.csv >> further-analysis.csv
 	tail -n +2 greedy-admissible.csv     >> further-analysis.csv
 	tail -n +2 astar-admissible.csv      >> further-analysis.csv
-	tail -n +2 dfs-greedy-fh.csv         >> further-analysis.csv
-	tail -n +2 dfs-fh.csv                >> further-analysis.csv
-	tail -n +2 astar-fh.csv              >> further-analysis.csv
+#	tail -n +2 dfs-greedy-fh.csv         >> further-analysis.csv
+#	tail -n +2 greedy-fh.csv             >> further-analysis.csv
+#	tail -n +2 astar-fh.csv              >> further-analysis.csv
 
 bfs.csv:
-	$(call run_analysis,bfs,bfs)
+	$(call run_analysis_short,bfs,bfs)
 
 dfs-greedy-admissible.csv:
 	$(call run_analysis,informed admissible dfs-greedy,dfs-greedy-admissible)
@@ -122,12 +147,12 @@ greedy-admissible.csv:
 astar-admissible.csv:
 	$(call run_analysis,informed admissible astar,astar-admissible)
 
-dfs-greedy-fh.csv:
-	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon dfs-greedy,dfs-greedy-fh)
+# dfs-greedy-fh.csv:
+# 	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon dfs-greedy,dfs-greedy-fh)
 
-dfs-fh.csv:
-	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon greedy,dfs-fh)
+# greedy-fh.csv:
+# 	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon greedy,greedy-fh)
 
-astar-fh.csv:
-	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon astar,astar-fh)
+# astar-fh.csv:
+# 	$(call run_analysis,informed finite-horizon-heuristics 3 admissible finite-horizon astar,astar-fh)
 
--- analysis/further-analysis/process.cpp
+++ analysis/further-analysis/process.cpp
@@ -13,9 +13,9 @@ int main(int argc, char *argv[]){
     char header[256];
     scanf("%s\n", header);
     map<string, map<tuple<lli,lli,lli,lli>, tuple<double,double,double>>> data;
-    char alg[256]; lli nTubes, tubeH, nColors, seed, nMoves, mem, t;
+    char alg[256]; lli nTubes, tubeH, nColors, seed; double nMoves, mem, t;
     while(scanf(
-            "%s ,%lld,%lld,%lld,%lld,%lld,%lld,%lld\n",
+            "%s ,%lld,%lld,%lld,%lld,%lf,%lf,%lf\n",
             alg, &nTubes, &tubeH, &nColors, &seed, &nMoves, &mem, &t
     ) != -1){
         data[string(alg)][make_tuple(nTubes, tubeH, nColors, seed)] = make_tuple(
@@ -26,21 +26,19 @@ int main(int argc, char *argv[]){
     }
     cerr << "Collected data" << endl;
     // Calculate optimality
-    for(const auto &p1: data){
+    for(auto &p1: data){
         string alg = p1.first;
         if(alg == "astar-admissible") continue;
-        for(const auto &p2: data.at(alg)){
-            tuple<lli,lli,lli,lli> testCase = p2.first;
-            get<1>(data.at(alg).at(testCase)) /= get<1>(data.at("astar-admissible").at(testCase));
+        for(auto &p2: p1.second){
+            auto testCase = p2.first;
+            get<0>(p2.second) = get<0>(data.at("astar-admissible").at(testCase))/get<0>(p2.second);
         }
     }
     cerr << "Calculated optimality for all except astar-admissible" << endl;
     {
         string alg = "astar-admissible";
-        for(const auto &p: data) cerr << p.first << endl;
-        for (const auto &p2: data.at(alg)) {
-            tuple<lli, lli, lli, lli> testCase = p2.first;
-            get<1>(data.at(alg).at(testCase)) = 1;
+        for (auto &p2: data.at(alg)) {
+            get<0>(p2.second) = 1.0;
         }
     }
     cerr << "Calculated optimality" << endl;
@@ -98,8 +96,8 @@ int main(int argc, char *argv[]){
                 << alg
                 << "," << get<0>(testCase) << "," << get<1>(testCase) << "," << get<2>(testCase)
                 << "," << nMovesMin.at(testCase) << "," << nMovesMean.at(testCase) << "," << nMovesMax.at(testCase)
-                << "," << nMovesMin.at(testCase) << "," << nMovesMean.at(testCase) << "," << nMovesMax.at(testCase)
-                << "," << nMovesMin.at(testCase) << "," << nMovesMean.at(testCase) << "," << nMovesMax.at(testCase)
+                << "," << memMin   .at(testCase) << "," << memMean   .at(testCase) << "," << memMax   .at(testCase)
+                << "," << tMin     .at(testCase) << "," << tMean     .at(testCase) << "," << tMax     .at(testCase)
                 << endl;
         }
     }
