8b262fbe4192792db95e9274c004162a870f691e
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2021-04-02 01:26:22 +0100

    Changed stuff

--- analysis/discard-dfs/discard-dfs.mk
+++ analysis/discard-dfs/discard-dfs.mk
@@ -11,11 +11,21 @@ define run_analysis
 	$(MAIN) cli $(NRUNS) 7 4 4 0 $(1) >> $(2).csv
 endef
 
+define run_analysis_ids
+	echo "nTubes,tubeH,nColors,seed,$(2)-nMoves,$(2)-mem_b,$(2)-t_ns" > $(2).csv
+	$(MAIN) cli $(NRUNS) 2 4 1 0 $(1) >> $(2).csv
+	$(MAIN) cli $(NRUNS) 3 4 1 0 $(1) >> $(2).csv
+	$(MAIN) cli $(NRUNS) 4 4 2 0 $(1) >> $(2).csv
+	$(MAIN) cli $(NRUNS) 5 4 3 0 $(1) >> $(2).csv
+	echo ",,,,,," >> $(2).csv
+	echo ",,,,,," >> $(2).csv
+endef
+
 all: discard-dfs-math.csv
 
 discard-dfs-math.csv: discard-dfs.csv
 	head -n 1 discard-dfs.csv > discard-dfs-math.csv
-	cat discard-dfs.csv | tail -n +2 | awk -F , -v OFS=, '{print $$1,$$2,$$3,$$4,$$20/$$5,$$6,$$7/$$10,$$20/$$8,$$9,$$10/1000000,$$20/$$11,$$12,$$13/$$10,$$20/$$14,$$15,$$16/$$10,$$20/$$17,$$18,$$19/$$10,$$20,$$21,$$22/$$10,$$20/$$23,$$24,$$25/$$10,$$20/$$26,$$27,$$28/$$10,$$20/$$29,$$30,$$31/$$10}' >> discard-dfs-math.csv
+	cat discard-dfs.csv | tail -n +2 | awk -F , -v OFS=, '{print $$1,$$2,$$3,$$4,$$20/$$5,$$6/1000000,$$7/$$10,$$20/$$8,$$9/1000000,$$10/1000000,($$11==""?"-":$$20/$$11),($$12==""?"-":$$12/1000000),($$13==""?"-":$$13/$$10),$$20/$$14,$$15/1000000,$$16/$$10,$$20/$$17,$$18/1000000,$$19/$$10,$$20,$$21/1000000,$$22/$$10,$$20/$$23,$$24/1000000,$$25/$$10,$$20/$$26,$$27/1000000,$$28/$$10,$$20/$$29,$$30/1000000,$$31/$$10}' >> discard-dfs-math.csv
 
 discard-dfs.csv: dfs.csv bfs.csv iterative-deepening.csv dfs-greedy-admissible.csv greedy-admissible.csv astar-admissible.csv dfs-greedy-fh.csv dfs-fh.csv astar-fh.csv
 	csvcut -c 5,6,7 bfs.csv                     > bfs-cut.csv
@@ -45,7 +55,7 @@ bfs.csv:
 	$(call run_analysis,bfs,bfs)
 
 iterative-deepening.csv:
-	$(call run_analysis,iterative-deepening,iterative-deepening)
+	$(call run_analysis_ids,iterative-deepening,iterative-deepening)
 
 dfs-greedy-admissible.csv:
 	$(call run_analysis,informed admissible dfs-greedy,dfs-greedy-admissible)
